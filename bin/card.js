7f318657428acbfdfdc8ddc6bc58e6c9291603eb
JIBIN-P <philiposejibin@gmail.com>
2020-04-24 09:18:14 +0530

    fixed breaking changes.

--- bin/card.js
+++ bin/card.js
@@ -1,54 +1,53 @@
-#! / usr / bin / env node
+#!/usr/bin/env node
 // Used to tell Node.js that it is a CLI tool
 'use strict';
 
 // Declaring the modules:
-import { white, cyan, green, rgb } from ' chalk ';
-import boxen from ' boxen ';
+const chalk = require('chalk');
+const boxen = require('boxen');
 
 // Setting options for 'boxen':
 const options = {
-  padding: 7,
-  margin: 7,
+  padding: 1,
+  margin: 1,
   borderStyle: 'double',
   float: 'center',
-  borderColor: '# ff4879',
+  borderColor: '#ff4879',
   backgroundColor: 'black',
 };
 
 // Inclusion of Text and more 'chalk' definitions:
 const data = {
-  name: white(' Jibin Philipose / '),
-  handle: cyan(' JIBIN-P '),
-  work: white(' Full Stack Web Developer At TSS Consultancy '),
-  twitter: gray('https://twitter.com/') + cyan('JIBIN_TP'),
-  github: gray('https://github.com/') + green('JIBIN-P'),
-  linkedIn: gray('https://linkedin.com/in/') + blue('jibin-p'),
-  medium: cyan(' https://medium.com/@jibin_p '),
-  npx: red('npx') + ' ' + white('jibin-p'),
-  labelWork: rgb(255, 0, 0).bold('Work:'),
-  labelTwitter: rgb(0, 255, 255).bold('Twitter:'),
-  labelGitHub: rgb(253, 245, 5).bold('GitHub:'),
-  labelLinkedIn: rgb(51, 51, 255).bold('LinkedIn:'),
-  labelMedium: rgb(102, 51, 0).bold('Medium:'),
+  name: chalk.white('Jibin Philipose/'),
+  handle: chalk.cyan('JIBIN-P'),
+  work: chalk.white('Full Stack Web Developer At TSS Consultancy.'),
+  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('JIBIN_TP'),
+  github: chalk.gray('https://github.com/') + chalk.green('JIBIN-P'),
+  linkedIn: chalk.gray('https://linkedin.com/in/') + chalk.blue('jibin-p'),
+  medium: chalk.cyan(' https://medium.com/@jibin_p '),
+  npx: chalk.red('npx') + ' ' + chalk.white('jibin-p'),
+  labelWork: chalk.rgb(255, 0, 0).bold('Work:'),
+  labelTwitter: chalk.rgb(0, 255, 255).bold('Twitter:'),
+  labelGitHub: chalk.rgb(253, 245, 5).bold('GitHub:'),
+  labelLinkedIn: chalk.rgb(51, 51, 255).bold('LinkedIn:'),
+  labelMedium: chalk.rgb(102, 51, 0).bold('Medium:'),
   labelCard: chalk.rgb(240, 13, 240).bold('Card:'),
 };
 
 // Here is the output of our Personal Card in NPX:
-const newline = ' \n ';
-const heading = ` $ { data.name }  $ { data.handle } `;
-const working = ` $ { data.labelWork }   $ { data.work } `;
-const twittering = ` $ { data.labelTwitter }   $ { data.twitter } `;
-const githubData = ` $ { data.labelGitHub }   $ { data.github } `;
-const linkedInData = ` $ { data.labelLinkedIn }   $ { data.linkedIn } `;
-const mediumData = ` $ { data.labelMedium }   $ { data.medium } `;
-const carding = ` $ { data.labelCard }   $ { data.npx } `;
+const newline = '\n';
+const heading = `${data.name}${data.handle}`;
+const working = ` ${data.labelWork}  ${data.work}`;
+const twittering = ` ${data.labelTwitter}  ${data.twitter}`;
+const githubData = ` ${data.labelGitHub}  ${data.github}`;
+const linkedInData = ` ${data.labelLinkedIn}  ${data.linkedIn}`;
+const mediumData = ` ${data.labelMedium}  ${data.medium}`;
+const carding = ` ${data.labelCard}  ${data.npx}`;
 
 // Here we must put all our output in a single variable so that we can use
 // the 'boxen effectively:
 const output = [
   heading,
-  newline,
   newline,
   working,
   newline,
@@ -60,8 +59,7 @@ const output = [
   newline,
   mediumData,
   newline,
-  newline,
   carding,
 ].join(newline);
 
-console.log(green(boxen(output, options)));
+console.log(chalk.green(boxen(output, options)));
